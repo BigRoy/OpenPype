033491f55795aa5a8383deb053c8c34fdd16b3cf
Roy Nieterau <roy_nieterau@hotmail.com>
2022-12-12 00:14:06 +0100

    Add resources/scripts/houdinipath to pythonpath

--- openpype/hosts/houdini/hooks/set_houdini_job_resources.py
+++ openpype/hosts/houdini/hooks/set_houdini_job_resources.py
@@ -2,6 +2,14 @@ import os
 from openpype.lib import PreLaunchHook
 
 
+def add_path_to_env(env, key, path):
+    existing = env.get(key)
+    if existing:
+        env[key] = os.pathsep.join([existing, path])
+    else:
+        env[key] = path
+
+
 class HoudiniSetJobResourcesEnvs(PreLaunchHook):
     """Set environment variable JOB and HOUDINI_OTLSCAN_PATH.
 
@@ -22,8 +30,15 @@ class HoudiniSetJobResourcesEnvs(PreLaunchHook):
         project_root_work = os.path.join(work_root, project_name)
         project_resources = os.path.join(project_root_work, "resources")
         project_resource_otls = os.path.join(project_resources, "otls")
+        project_resources_pythonpath = os.path.join(project_resources,
+                                                    "scripts",
+                                                    "houdinipath")
 
         self.launch_context.env.update({
-            "JOB": f"{project_resources}",
+            "JOB": project_resources,
             "HOUDINI_OTLSCAN_PATH": f"{project_resource_otls};&"
         })
+
+        add_path_to_env(env=self.launch_context.env,
+                        key="PYTHONPATH",
+                        path=project_resources_pythonpath)

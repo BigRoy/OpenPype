9339fec5ce1b1ef332df56512923033af7450a1a
Roy Nieterau <roy@colorbleed.nl>
2023-06-01 13:19:34 +0200

    Set C4DPYTHONPATH310 for recent C4D versions

--- openpype/hosts/cinema4d/__init__.py
+++ openpype/hosts/cinema4d/__init__.py
@@ -19,24 +19,27 @@ def add_implementation_envs(env, _app):
 
     env["g_additionalModulePath"] = os.pathsep.join(new_plugins_paths)
 
-
-    # Add requirements to the PYTHONPATH
-
-    pythonpath_key = "PYTHONPATH"
-
-    if int(_app.name) == 23:
-        
-        pythonpath_key = "C4DPYTHONPATH37"
-
-    elif int(_app.name) > 23:
-
+    # Add requirements to the PYTHONPATH. Recent version of Cinema4D do not
+    # add the PYTHONPATH to sys.path internally and thus require to explicitly
+    # have the right pythonpaths set up with a dedicated variable.
+    app_version = int(_app.name)
+    print("Detected app version: {}".format(app_version))
+    if app_version >= 2023:
+        # C4D updated Python 3.10.8 in 2023.2
+        pythonpath_key = "C4DPYTHONPATH310"
+    elif app_version > 23:
         pythonpath_key = "C4DPYTHONPATH39"
+    elif app_version == 23:
+        pythonpath_key = "C4DPYTHONPATH37"
+    else:
+        # Legacy
+        pythonpath_key = "PYTHONPATH"
 
     new_c4dpython_path = [
         os.path.join(pype_root, "vendor", "python")
     ]
 
-    # Add any existing requirements form the c4d python path env var
+    # Add any existing requirements from the c4d python path env var
     old_c4dpython_path = env.get(pythonpath_key) or ""
     for path in old_c4dpython_path.split(os.pathsep):
         if not path:
@@ -56,9 +59,7 @@ def add_implementation_envs(env, _app):
         if norm_path not in new_plugins_paths:
             new_c4dpython_path.append(norm_path)
 
-
     env[pythonpath_key] = os.pathsep.join(new_c4dpython_path)
-
 
     # C4D's python ships without python3.dll which pyside expects
     if "win" in env.get("PLAT"):

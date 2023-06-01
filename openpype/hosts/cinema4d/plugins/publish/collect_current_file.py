671a763e3b9332038f539dcf66841f38d6bd2153
Roy Nieterau <roy@colorbleed.nl>
2023-06-01 12:46:02 +0200

    Cosmetics

--- openpype/hosts/cinema4d/plugins/publish/collect_current_file.py
+++ openpype/hosts/cinema4d/plugins/publish/collect_current_file.py
@@ -1,9 +1,8 @@
-import c4d
-
 import pyblish.api
 from openpype.hosts.cinema4d import api
 
-class CollectCinema4DrCurrentFile(pyblish.api.ContextPlugin):
+
+class CollectCinema4DCurrentFile(pyblish.api.ContextPlugin):
     """Inject the current working file into context"""
 
     order = pyblish.api.CollectorOrder - 0.5
@@ -15,5 +14,6 @@ class CollectCinema4DrCurrentFile(pyblish.api.ContextPlugin):
 
         context.data['currentFile'] = api.current_file()
 
-        assert api.current_file() != '', "Current file is empty. " \
-            "Save the file before continuing."
+        assert api.current_file() != '', (
+            "Current file is not saved. Save the file before continuing."
+        )

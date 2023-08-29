b50c8a9c9b68ac83f94cb3fac5666a474b733b74
Roy Nieterau <roy_nieterau@hotmail.com>
2023-08-29 12:38:20 +0200

    Cosmetics

--- openpype/modules/deadline/plugins/publish/validate_deadline_pools.py
+++ openpype/modules/deadline/plugins/publish/validate_deadline_pools.py
@@ -24,7 +24,6 @@ class ValidateDeadlinePools(OptionalPyblishPluginMixin,
                 "renderlayer",
                 "maxrender"]
     optional = True
-    
 
     # cache
     pools_per_url = {}

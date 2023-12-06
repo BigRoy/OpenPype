c50a8636477ebf9b250e90cc3f32a0bbc1b9266f
Roy Nieterau <roy_nieterau@hotmail.com>
2023-12-06 16:44:37 +0100

    Fix support for `.tif` with single `f`

--- openpype/tools/standalonepublish/widgets/widget_drop_frame.py
+++ openpype/tools/standalonepublish/widgets/widget_drop_frame.py
@@ -20,9 +20,9 @@ class DropDataFrame(QtWidgets.QFrame):
         ".jpeg", ".xt", ".jpeg-hdr", ".kra", ".mng", ".miff", ".nrrd",
         ".ora", ".pam", ".pbm", ".pgm", ".ppm", ".pnm", ".pcx", ".pgf",
         ".pictor", ".png", ".psb", ".psp", ".qtvr", ".ras",
-        ".rgbe", ".logluv", ".tiff", ".sgi", ".tga", ".tiff", ".tiff/ep",
-        ".tiff/it", ".ufo", ".ufp", ".wbmp", ".webp", ".xbm", ".xcf",
-        ".xpm", ".xwd"
+        ".rgbe", ".logluv", ".tiff", ".sgi", ".tga", ".tif", ".tiff",
+        ".tiff/ep", ".tiff/it", ".ufo", ".ufp", ".wbmp", ".webp", ".xbm",
+        ".xcf", ".xpm", ".xwd"
     ]
     video_extensions = [
         ".3g2", ".3gp", ".amv", ".asf", ".avi", ".drc", ".f4a", ".f4b",
